cf8f1081cdb1fa975dc9db8c07ea08ebe800c3a6
Coder-Jojo <nikhilchaudhary1243@gmail.com>
2021-07-21 18:48:46 +0530

    removed uri cred

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 import postRoutes from './routes/posts.js';
 
@@ -13,7 +16,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors);
 
-const CONNECTION_URL = 'mongodb+srv://jojo:jojo@cluster0.tntnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.MONGO_URI
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
